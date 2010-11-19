460d789440c4eda1d34c14df111ecf68e521f254
Bryan Clark <clarkbw@gmail.com>
2010-11-18 23:58:19 -0800

    really simple start to the api server.  next up is the alias requests

--- api/server.js
+++ api/server.js
@@ -1,49 +1,36 @@
 
-var redis = require("../node_redis/"),
+var redis = require("../lib/node_redis"),
     client = redis.createClient();
 
 client.on("error", function (err) {
-    console.log("Redis connection error to " + client.host + ":" + client.port + " - " + err);
+  console.log("Redis connection error to " + client.host + ":" + client.port + " - " + err);
 });
 
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    client.quit();
+var express = require('../lib/express');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
 });
 
-var journey = require('../journey/lib/journey');
-
-//
-// Create a Router object with an associated routing table
-//
-var router = new(journey.Router)(function (map) {
-    map.root.bind(function (res) { res.sendBody(JSON.stringify({hello:"world"})); } );
-    map.get(/^conversation\/([0-9]+)$/).bind(function (res, id) {
-
-    });
-    map.post('/conversations').bind(function (res, data) {
-        sys.puts(data.type); // "Cave-Troll"
-        res.send(200);
-    });
-});
-
-require('http').createServer(function (request, response) {
-    var body = "";
-
-    request.addListener('data', function (chunk) { body += chunk });
-    request.addListener('end', function () {
-        //
-        // Dispatch the request to the router
-        //
-        router.route(request, body, function (result) {
-            response.writeHead(result.status, result.headers);
-            response.end(result.body);
+app.get('/conversation/:id', function (req, res, next) {
+    // Grab the conversation from the db
+    var id = req.params.id;
+    if (id) {
+        client.zrange("conversation:" + id + ":messages", 0, -1, function (err, messages) {
+            console.log(" messages: " + messages.length);
+            console.log("messages: " + messages);
+            msgs = new Array(messages.length);
+            messages.forEach(function(message, i) {
+                console.log("message: " + message);
+               msgs.push(message + ""); 
+            });
+            res.send(msgs);
         });
-    });
-}).listen(8080);
+    } else {
+        next(new Error('Failed to load user ' + req.params.id));
+    }
+});
+
+app.listen(8080);
+
